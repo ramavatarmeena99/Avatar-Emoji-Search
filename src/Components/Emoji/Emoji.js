f94a4d2dbd5de1b27f9ab693453910e6661a714b
Ramavatar Meena <ramavtarmeena@MacBook-Air.local>
2022-12-29 10:30:47 +0530

    emoji add

--- src/Components/Emoji/Emoji.js
+++ src/Components/Emoji/Emoji.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { dataEmoji } from '../Data/DataEmoji'
 
 export default function Emoji() {
   return (
-    <div>Emoji</div>
+    <div>
+        {dataEmoji.map((i,index)=>{
+            return(
+            <div style={{border:"1px solid black", display:"flex"}}>
+                <p>{i.title}</p>
+                <p>{i.symbol}</p>
+
+
+            </div>
+                )
+        })}
+
+  </div>
   )
 }
