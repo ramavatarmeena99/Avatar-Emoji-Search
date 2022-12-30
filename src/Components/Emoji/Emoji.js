3f486e19a0663925f10268dcd48f3b03ed0591f3
Ramavatar Meena <ramavtarmeena@MacBook-Air.local>
2022-12-30 14:35:04 +0530

    some issue fixed

--- src/Components/Emoji/Emoji.js
+++ src/Components/Emoji/Emoji.js
@@ -1,20 +1,17 @@
-import React from 'react'
-import { dataEmoji } from '../Data/DataEmoji'
+import React from "react";
+import { dataEmoji } from "../Data/DataEmoji";
 
 export default function Emoji() {
   return (
     <div>
-        {dataEmoji.map((i,index)=>{
-            return(
-            <div style={{border:"1px solid black", display:"flex"}}>
-                <p>{i.title}</p>
-                <p>{i.symbol}</p>
-
-
-            </div>
-                )
-        })}
-
-  </div>
-  )
+      {dataEmoji.map((i, index) => {
+        return (
+          <div style={{ border: "1px solid black", display: "flex" }}>
+            <p>{i.symbol}</p>
+            <p>{i.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
---
+++ src/Components/EmojiResultRow/EmojiResultRow.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// export default function EmojiResultRow({title}) {
+//   const codePointHex = symbol.codePointAt(0).toString(16);
+//   const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
+//   return (
+//     <div
+
+//   >
+//     <img alt={title} src={src} />
+//     <span className="title">{title}</span>
+//     <span className="info">Click to copy emoji</span>
+//   </div>
+//   )
+// }
