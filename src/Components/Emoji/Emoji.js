aeb74146a45686c71aa43759d1fa05a7f4fe4c61
Ramavatar Meena <ramavtarmeena@MacBook-Air.local>
2022-12-30 15:03:25 +0530

    search button add

--- src/Components/Emoji/Emoji.js
+++ src/Components/Emoji/Emoji.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { dataEmoji } from "../Data/DataEmoji";
 
 export default function Emoji() {
+  const [searchInput, setSearchInput] = useState("");
+  const [filterResult, setFilterResult] = useState([]);
+  const search = (e) => {
+    setSearchInput(e.target.value);
+    if (searchInput !== "") {
+      const filterData = dataEmoji.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+
+      setFilterResult(filterData);
+    } else {
+      setFilterResult(dataEmoji);
+    }
+  };
   return (
     <div>
-      {dataEmoji.map((i, index) => {
-        return (
-          <div style={{ border: "1px solid black", display: "flex" }}>
-            <p>{i.symbol}</p>
-            <p>{i.title}</p>
-          </div>
-        );
-      })}
+      <input
+        style={{ width: "300px", height: "50px", fontSize: "17px" }}
+        type="text"
+        placeholder="search"
+        //   value={query}
+        onChange={search}
+      ></input>
+
+      {searchInput.length > 1
+        ? filterResult.map((i, index) => {
+            return (
+              <div
+                key={index}
+                style={{ border: "1px solid black", display: "flex" }}
+              >
+                <p>{i.symbol}</p>
+                <p>{i.title}</p>
+              </div>
+            );
+          })
+        : dataEmoji.map((item, index) => {
+            return (
+              <div key={index} style={{ display: "flex", paddingTop: "10px" }}>
+                <p>{item.title}</p>
+                <p>{item.symbol}</p>
+              </div>
+            );
+          })}
     </div>
   );
 }
---
+++ src/Components/SearchInput.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+// export default function SearchInput() {
+//   return (
+//     <div>
+//     <div>
+//       <input onChange={this.handleChange} />
+//     </div>
+//   </div>
+//   )
+// }
